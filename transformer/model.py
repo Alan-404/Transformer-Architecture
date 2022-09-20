349de15e2fa42a7245cf777bb39d7beb5e705c09
alan-404 <nguyentri.alan@gmail.com>
2022-09-20 09:57:06 +0700

    fix

--- transformer/model.py
+++ transformer/model.py
@@ -7,9 +7,9 @@ from keras.metrics import Mean
 from keras.optimizers import Adam
 import tensorflow as tf
 
-class Transformer(Model):
+class TransformerModel(Model):
     def __init__(self, n, h, input_vocab_size, target_vocab_size, d_model, d_ff, activation, dropout_rate=0.1, eps=0.1):
-        super(Transformer, self).__init__()
+        super(TransformerModel, self).__init__()
         self.encoder = Encoder(h, d_ff, d_model, input_vocab_size, activation, n, dropout_rate, eps)
         self.decoder = Decoder(h, d_ff, d_model, target_vocab_size, activation, n, dropout_rate, eps)
         self.ffn = ffn(d_ff, d_model, activation)
